8ff4f1ac891f8363f65798559a80b5bb77a10506
Christine Wilks <crissxross@users.noreply.github.com>
2025-03-16 13:01:09 +0000

    Add mapRange and constrain functions to utility helpers for value mapping and range constraining

--- canvas-cx/utils/helpers.js
+++ canvas-cx/utils/helpers.js
@@ -21,3 +21,23 @@ function distance(p1, p2) {
   // return Math.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2);
   return Math.hypot(p1.x-p2.x, p1.y-p2.y);
 }
+
+// map a value from one range to another
+/**
+ *
+ * @param {*} value: Number - The value that should be mapped (typically it's between inMin and inMax).
+ * @param {*} inMin: Number - The lower bound of the initial range to map from
+ * @param {*} inMax: Number - The upper bound of the initial range to map from
+ * @param {*} outMin: Number - The lower bound of the range to map to
+ * @param {*} outMax: Number - The upper bound of the range to map to
+ * @returns Number - The mapped value
+ */
+
+function mapRange (value, inMin, inMax, outMin, outMax){
+  return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+// constrain a value to a range
+function constrain(value, min, max) {
+  return Math.min(max, Math.max(min, value));
+}
